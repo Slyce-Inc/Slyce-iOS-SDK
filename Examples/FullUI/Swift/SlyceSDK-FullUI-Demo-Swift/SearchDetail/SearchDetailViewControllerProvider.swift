55513cd4f79534cd0c21fc9bc36c01f3d56237fc
Andrew Mroczkowski <amrox@users.noreply.github.com>
2018-05-10 10:58:29 -0400

    Simple custom search detail example (#9)
    * Adding same example for custom search detail
    
    * Adding missing files
    
    * Code formatting
    
    * Typo

---
+++ Examples/FullUI/Swift/SlyceSDK-FullUI-Demo-Swift/SearchDetail/SearchDetailViewControllerProvider.swift
@@ -0,0 +1,12 @@
+import Foundation
+import SlyceSDK
+import SlyceSDK.Private
+
+class SearchDetailViewControllerProvider: NSObject, SlyceCustomSearchDetailViewControllerProvider {
+    
+    func searchDetailViewController(forResultItem resultItem: [AnyHashable : Any]) -> UIViewController? {
+        let vc = SearchDetailViewController();
+        vc.slyceResultItem = resultItem;
+        return vc
+    }
+}
