0b9cbbdd2a9faf0e774187e58f0f91f9611965fb
Andrew Mroczkowski <amrox@users.noreply.github.com>
2018-08-01 19:07:09 -0400

    Updating Examples

--- Examples/FullUI/SlyceSDK-FullUI-Demo-Swift/SearchDetail/SearchDetailViewController.swift
+++ Examples/FullUI/SlyceSDK-FullUI-Demo-Swift/SearchDetail/SearchDetailViewController.swift
@@ -1,13 +1,32 @@
+
 import UIKit
 
 class SearchDetailViewController: UIViewController {
-
-    var slyceResultItem: [AnyHashable: Any]?
+    
+    @IBOutlet weak var itemTextView: UITextView?
+    
+    var item: [AnyHashable: Any]? {
+        didSet {
+            updateItemTextView()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // build your view with the result from Slyce here
-        
+        updateItemTextView()
     }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func close(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    fileprivate func updateItemTextView() {
+        self.itemTextView?.text = self.item?.description
+    }
+
 }
